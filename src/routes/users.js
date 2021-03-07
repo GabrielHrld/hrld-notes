20eb465e59ee75668c3ee254ec2d257c830849db
Gabi <g.federico.rodriguez@gmail.com>
2021-03-07 14:04:57 -0300

    sign up

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,54 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get('/users/signin', (req, res) => {
-  res.render('users/signin.hbs')
-})
+router.get("/users/signin", (req, res) => {
+  res.render("users/signin.hbs");
+});
 
-router.get('/users/signup', (req, res) => {
-  res.render('users/signup.hbs')
-})
+router.get("/users/signup", (req, res) => {
+  res.render("users/signup.hbs");
+});
+
+router.post("/users/signup", async (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
+  const errors = [];
+  if (name.length <= 0) {
+    errors.push({ text: "Please insert your name" });
+  }
+  if (email.length <= 0) {
+    errors.push({ text: "Please insert your email" });
+  }
+  if (password.length <= 0) {
+    errors.push({ text: "Please insert an password" });
+  }
+  if (password != confirm_password) {
+    errors.push({ text: "Password does not match" });
+  }
+  if (password.length < 4) {
+    errors.push({ text: "Password must be at least 4 characters" });
+  }
+  if (errors.length > 0) {
+    res.render("users/signup", {
+      errors,
+      name,
+      email,
+      password,
+      confirm_password,
+    });
+  } else {
+    const user = await User.findOne({ email: email });
+    console.log(user.email);
+    if (user.email === email) {
+      req.flash("error_msg", "The email is already in use");
+      res.redirect("/users/signup");
+    } else {
+      const newUser = new User({ name, email, password });
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash("success_msg", "You are registered");
+      res.redirect("/users/signin");
+    }
+  }
+});
 
 module.exports = router;
