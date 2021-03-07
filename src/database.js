d1e77ad3112b7fa4492adfa46ccead2d5c43bf26
Gabi <g.federico.rodriguez@gmail.com>
2021-03-07 09:56:13 -0300

    alert messages

--- src/database.js
+++ src/database.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const URI =
   "mongodb+srv://db_user_gabi:crazylife013@cluster0.s5abe.mongodb.net/test";
 
+//DATABASE connection
 mongoose
   .connect(URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then((db) => console.log("[DB] is connected"))
   .catch((error) => console.error(error));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
 
-router.get('/notes', (req, res) => {
-  res.send('notes view')
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note.hbs");
+});
+
+router.post('/notes/new-note', (req, res) => {
+  const {title, description} = req.body;
+  const errors = []
+  if(!title){
+    errors.push({text: 'Please write a Title'})
+  }
+  if(!description){
+    errors.push({text: 'Please write a Description'})
+  }
+  if(errors.length > 0 ){
+    res.render('notes/new-note', {
+      errors,
+      title,
+      description
+    })
+  } else {
+    res.send('ok')
+  }
 })
+
+router.get("/notes", (req, res) => {
+  res.send("notes view");
+});
 
 module.exports = router;
